52d6553ba8d94ddace5ccbd41afa5b3c8537d300
Marco Angioli <marco.angioli@uniroma1.it>
2024-05-17 17:07:34 +0200

    Update model.cpp

--- src/model/cpp/model.cpp
+++ src/model/cpp/model.cpp
@@ -1,3 +1,32 @@
+/**
+ * @file Model.cpp
+ * @author Marco Angioli and Saeid Jamili
+ * @email marco.angioli@uniroma1.it and saeid.jamili@uniroma1.it
+ * @date Created on: 12th August 2023
+ * @date Last updated on: 16th August 2023
+ * @institution Sapienza University of Rome
+ * @ref :
+ * https://doi.org/10.xxxx/yyyyy
+ *
+ * @section LICENSE
+    Copyright 2024 Sapienza University of Rome
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+    Authors: Marco Angioli & Saeid Jamili
+
+
+ */
+"""
 #include "./classes/HDC_op.hpp"
 
 
